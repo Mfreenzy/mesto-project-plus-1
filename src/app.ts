c6abb453e829622b846bc13241469d700a953705
Sergei Fedorchenko <masterserg2014@yandex.ru>
2024-03-23 20:29:04 +0300

    Проверка в Postman

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
-import express, { Response } from 'express';
-import mongoose from 'mongoose';
-import { IRequest } from './types/types';
-import routes from './routes/index';
+import express, { Response } from "express";
+import mongoose from "mongoose";
+import { IRequest } from "./types/types";
+import routes from "./routes/index";
 
 const app = express();
 const PORT = 3000;
@@ -10,20 +10,23 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Подключение к серверу MongoDB
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-}).then(() => {
-  console.log('Подключение к MongoDB успешно');
-}).catch((error) => {
-  console.error('Ошибка подключения к MongoDB:', error);
-});
+mongoose
+  .connect("mongodb://localhost:27017/mestodb", {})
+  .then(() => {
+    console.log("Подключение к MongoDB успешно");
+  })
+  .catch((error) => {
+    console.error("Ошибка подключения к MongoDB:", error);
+  });
 
 app.use((req: IRequest, res: Response, next) => {
-  req.user = { _id: '64442f4253e0cd4e2d79990d' };
+  req.user = { _id: "65ff0f0def7abb4a3aa6a469" };
 
   next();
 });
 
 app.use(routes);
+
 app.listen(PORT, () => {
   console.log(`Сервер запущен на порту ${PORT}`);
 });
